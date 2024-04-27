833063b54d0ecda7c9b0b87d68c4138b7de3e316
max46q <2000maksym2000@gmail.com>
2024-04-28 01:27:32 +0300

    надіюсь на vercel

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ require("dotenv").config();
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 //
-
 //
 app.use(express.static(__dirname + "/views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
-
 
 app.get("/", function (req, res) {
   let error = "";
