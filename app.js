81ce905f260ee9d15d7e2e8111673b2187ba13bb
max46q <2000maksym2000@gmail.com>
2024-05-06 22:33:26 +0300

    test connected git and vs code

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 //
+
 //
 app.use(express.static(__dirname + "/views"));
 app.use(bodyParser.urlencoded({ extended: false }));
